a2da67d3a92017e03fd0528094b5863b76246d00
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2023-12-08 23:49:02 -0500

    missing files

---
+++ src/main/java/com/practice/shopv3api/ShopV3ApiApplication.java
@@ -0,0 +1,13 @@
+package com.practice.shopv3api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopV3ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopV3ApiApplication.class, args);
+	}
+
+}
