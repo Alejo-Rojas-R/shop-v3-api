a005dd1c2219fb7a7294de101d7c772551e87a71
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2024-02-08 12:57:05 -0500

    refactor: adding diagrams folder

--- src/test/java/com/practice/shopv3api/ShopV3ApiApplicationTests.java
+++ src/test/java/com/practice/shopv3api/ShopV3ApiApplicationTests.java
@@ -1,13 +1,7 @@
 package com.practice.shopv3api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShopV3ApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
